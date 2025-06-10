66a9c6ec6c7d75b19c3cb6db2cb0b30605f28888
Isaac Emmanuel <isaacnsisong@gmail.com>
2025-06-10 01:00:25 +0100

    i updated the details

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,15 +1,5 @@
-// /** @type {import('tailwindcss').Config} */
-// module.exports = {
-//   content: [
-//     "./src/**/*.{js,jsx,ts,tsx}",
-//   ],
-//   theme: {
-//     extend: {},
-//   },
-//   plugins: [],
-// }
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
